872b60979fe2cc69780908eb5bfb0db9fd39b0dd
Stephen Thurman <thurman024@gmail.com>
2018-01-31 03:30:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,8 @@ class Transfer
       @receiver.deposit(@amount)
       @status = "complete"
     else
-      "Transaction rejected. Please check your account balance."
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."  
     end
   end
 
